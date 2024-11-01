bba5b0ba6b171e0960f52070e814d02564e2c591
xieyuhua <1130107545@qq.com>
2024-11-01 08:58:05 +0800

    Update client.go

--- client/client.go
+++ client/client.go
@@ -9,22 +9,41 @@ import (
 	"io"
 	"net"
 	"os"
+	"time"
 	"os/exec"
 	"os/signal"
 	"runtime"
 	"syscall"
 	// 	"github.com/songgao/water"
 	"github.com/labulakalia/water"
+	"context"
+	"log"
+	_ "github.com/wzshiming/anyproxy/init"
+	_ "github.com/wzshiming/anyproxy/pprof"
+	"github.com/wzshiming/anyproxy"
+	"github.com/FlowerWrong/kone/tcpip"
 )
 
 var (
-	inSer = flag.String("ser", "47.105.115.26:8006", "server address")
-	inDev = flag.String("dev", "gtun", "local tun device name")
-	ip    = flag.String("ip", "10.10.10.1/24", "子网掩码是 255.255.255.0、 10.10.10.1/24")
+	inSer    = flag.String("ser", "47.105.115.26:8006", "server address")
+	inDev    = flag.String("dev", "gtun", "local tun device name")
+	port     = flag.String("port", ":1080", "proxy :1080")
+        proxy    = flag.Bool("proxy",  false, "默认关闭内置代理")
+	ip       = flag.String("ip", "10.10.10.1/24", "指定虚拟ip,指定的ip不能和其他设备重复,必须有效并且在服务端所属网段下, 子网掩码是 255.255.255.0、 10.10.10.1/24")
+	rule_in  = flag.String("i", "", "配置点对网(IP代理)时使用,-i 192.168.9.0/24,10.10.10.3表示允许接收网段192.168.9.0/24的数据，并转发到10.10.10.3(跳板机),可指定多个网段")
+	//ip route add 192.168.9.0/24 via 10.10.10.1
+	rule_out = flag.String("o", "", "配置点对网(跳板机)时使用,-o 10.10.10.0/24,表示来源10.10.10.0/24的数据通过nat映射后从gtun以外的其他网卡发出去")
+	//iptables -t nat -A POSTROUTING ! -o gtun -s 10.10.10.0/24 -j MASQUERADE
 )
+var logger = log.New(os.Stderr, "[server] ", log.LstdFlags)
 
 func main() {
 	flag.Parse()
+
+	//proxy server
+	if *proxy {
+	   go proxyServer(*port)
+	}
 
 	// 创建tun网卡
 	config := water.Config{
@@ -91,9 +110,36 @@ func main() {
 	<-sig
 }
 
+// 连接 proxy
+func proxyServer(address string) {
+	logger := log.New(os.Stderr, "[proxy server] ", log.LstdFlags)
+	var dialer net.Dialer
+	addrs := flag.Args()
+	if address != "" {
+		addrs = append(addrs, "http://"+address, "socks4://"+address, "socks5://"+address, "ssh://"+address, "pprof://"+address)
+	}
+	conf := anyproxy.Config{
+		Dialer: &dialer,
+		Logger: logger,
+	}
+	svc, err := anyproxy.NewAnyProxy(context.Background(), addrs, &conf)
+	if err != nil {
+		logger.Println(err)
+		return
+	}
+	logger.Printf("listen %s", addrs)
+	err = svc.Run(context.Background())
+	if err != nil {
+		logger.Println(err)
+	}
+	return
+}
+
 // 连接server
 func connServer(srv string) (conn net.Conn, err error) {
-	conn, err = net.Dial("tcp", srv)
+	//conn, err = net.Dial("tcp", srv)
+	// 设置超时时间为5秒  
+	conn, err = net.DialTimeout("tcp", srv, 3*time.Second)
 	if err != nil {
 		return nil, err
 	}
@@ -110,6 +156,23 @@ func ifceRead(ifce *water.Interface, conn net.Conn) {
 			color.Red(err.Error())
 			break
 		}
+		
+		//网关地址  判断请求数据ip 
+		if tcpip.IsIPv4(packet) {
+			ipPacket := tcpip.IPv4Packet(packet)
+			dstIP := ipPacket.DestinationIP()
+			srcIP := ipPacket.SourceIP()
+			logger.Printf("dstIP %v <= %v" , dstIP, srcIP)
+    		//是否当前子网
+            //_, subnet, err := net.ParseCIDR(*ip)
+    		//if err != nil {
+    		///	color.Red(err.Error())
+			//    break
+    		//}
+    		//if subnet.Contains(dstIP) {
+    		//}
+    	}
+		
 		// 转发到server端
 		err = forwardSer(conn, packet[:size])
 		if err != nil {
